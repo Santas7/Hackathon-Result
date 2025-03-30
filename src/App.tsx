a4c0ea04eee6d4aa037c98f606650e37bb04cf40
Santas7 <andreypanyshkin03@gmail.com>
2025-03-30 11:05:50 +0400

    upd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-import { GameArea } from './page/game-area'
+import { GameTimer } from './components/game-timer/game-timer'
+import { GameSetup } from './pages/game-setup'
+import { TestTimer } from './pages/test-timer/TestTimer'
 
 export const App = () => {
-  return <GameArea />
+
+  return (
+    <div>
+      {/* <GameSetup /> */}
+      {/* <GameTimer
+        time="01:22"
+        movesTaken={4}
+      /> */}
+      <TestTimer />
+    </div>
+  )
 }
